0d834341a1c201d4e4892a4e3bf64f4baad68c36
erik <emeier@nd.edu>
2021-07-15 15:42:21 -0500

    Button redirects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Box, Button } from '@material-ui/core'
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from 'react-router-dom';
+import { Button } from '@material-ui/core'
 import EnhancedTable from './inventoryTable';
-// Hello World.
+import requestOver from './requestOver'
+import { Domain } from '@material-ui/icons';
+
 function App() {
-  return ( 
-    <div className="App">
-      <header>
-        <h1>Full Circle</h1>
-      </header>
-      <div className="ButtonRow">
-        <Button variant="contained">
-          Make Request
-        </Button>
-        <Button variant="contained">
-          Pickups
-        </Button>
-      </div>
-      {EnhancedTable()}
+
+  return (
+    <div classname="App">
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/requests" component={requestOver} />
+      </Router>
     </div>
   )
 }
+
+const Home = () => (
+  <div>
+    <header>
+      <h1>Full Circle</h1>
+    </header>
+    <Router>
+      <Button variant="contained" href="/requests">
+        Make Request
+      </Button>
+      <Button variant="contained">
+        Pickups
+      </Button>
+    </Router>
+    {EnhancedTable()}
+  </div>
+);
 
 export default App;
